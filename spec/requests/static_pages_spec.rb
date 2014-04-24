a250eaf5e59104f7c63672535b677166ca1bd1e7
Ajico KIRIHARA <orangepillow@gmail.com>
2014-04-24 15:16:50 +0900

    fix spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "Static pages" do
     before { visit root_path }
     
     it { should have_content('Sample App') } 
-    it { should have_title(full_title('') }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
